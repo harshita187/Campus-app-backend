a148e939f188acd122e749455836434bd5c51ca7
harshita187 <harsita129singh@gmail.com>
2025-07-20 22:09:30 +0530

    phone added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,13 @@ const router = express.Router();
 
 // Signup Route
 router.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password,phone } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name, email, password: hashedPassword });
+    const user = new User({ name, email, password: hashedPassword,phone });
     await user.save();
 
     res.status(201).json({ message: "User created successfully" });
